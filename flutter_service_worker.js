3845649bf10578d653ff00f66d90984920cd9d8e
Lautaro Paletta <lautaropaletta1@gmail.com>
2023-11-05 23:43:36 -0300

    fourth commit

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -30,9 +30,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "3497b7cbccd28aa39f54d9f08904c0dc
 "flutter.js": "6b515e434cea20006b3ef1726d2c8894",
 "icons/Icon-192.png.png": "67e4f1ddc77ab1626783833d4b0a87d5",
 "icons/Icon-512.png": "7f7d2be3afec87b409403b33bc800505",
-"index.html": "10ba12fddca528993d699c6612b71d8d",
-"/": "10ba12fddca528993d699c6612b71d8d",
-"main.dart.js": "28f4b3f25fd11af960165e674de18f78",
+"index.html": "d050c16fe0da44bd47274007eaef9149",
+"/": "d050c16fe0da44bd47274007eaef9149",
+"main.dart.js": "efad9ebb96607065e06a664993108d55",
 "manifest.json": "9bd901577c8f17941128dd4cd638347b",
 "version.json": "43812052b4cc9c281f4f68cb6af844c1"};
 // The application shell files that are downloaded before a service worker can
